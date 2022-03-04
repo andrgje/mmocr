98228b14db93736bfe40ea8b9a4c9ebf938e807e
andrgje <aftgjesdal@gmail.com>
2022-03-04 13:59:01 +0100

    added configs

---
+++ configs/textdet/dbnet/dbnet_r50dcnv2_fpnc_1200e_pool_icdar2015_icdar2013.py
@@ -0,0 +1,35 @@
+_base_ = [
+    '../../_base_/runtime_10e.py',
+    '../../_base_/schedules/schedule_sgd_1200e.py',
+    '../../_base_/det_models/dbnet_r50dcnv2_fpnc.py',
+    '../../_base_/det_datasets/pool_icdar2013_icdar2015.py',
+    '../../_base_/det_pipelines/dbnet_pipeline.py'
+]
+
+train_list = {{_base_.train_list}}
+test_list = {{_base_.test_list}}
+
+train_pipeline_r50dcnv2 = {{_base_.train_pipeline_r50dcnv2}}
+test_pipeline_4068_1024 = {{_base_.test_pipeline_4068_1024}}
+
+load_from = '../../backbone/dbnet/dbnet_r50dcnv2_fpnc_sbn_2e_synthtext_20210325-aa96e477.pth'
+
+data = dict(
+    samples_per_gpu=1, #change from 8
+    workers_per_gpu=1, #changed from 8
+    val_dataloader=dict(samples_per_gpu=1),
+    test_dataloader=dict(samples_per_gpu=1),
+    train=dict(
+        type='UniformConcatDataset',
+        datasets=train_list,
+        pipeline=train_pipeline_r50dcnv2),
+    val=dict(   
+        type='UniformConcatDataset',
+        datasets=test_list,
+        pipeline=test_pipeline_4068_1024),
+    test=dict(
+        type='UniformConcatDataset',
+        datasets=test_list,
+        pipeline=test_pipeline_4068_1024))
+
+evaluation = dict(interval=100, metric='hmean-iou')
