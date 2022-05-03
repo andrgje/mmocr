3891dbfb104f43b24fba954d1078656f362931dd
andrgje <aftgjesdal@gmail.com>
2022-05-03 14:38:17 +0200

    change in test_script

--- test_show.py
+++ test_show.py
@@ -3,4 +3,4 @@ from mmocr.utils.ocr import MMOCR
 
 ocr=MMOCR(det_config='/home/andreas/Documents/Master/mmocr/configs/textdet/dbnet/dbnet_r50dcnv2_fpnc_1200e_icdar2015.py', det_ckpt='/home/andreas/Documents/Master/mmocr/checkpoints/textdet/dbnet/dbnet_r50dcnv2_fpnc_sbn_1200e_icdar2015_20211025-9fe3b590.pth')
 
-results = ocr.readtext('	test/test/',output='test/test_out/',export='test/test_out/')
+results = ocr.readtext('test/test/',output='test/test_out/',export='test/test_out/')
