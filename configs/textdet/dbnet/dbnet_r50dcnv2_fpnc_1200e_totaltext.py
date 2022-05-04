74e0317152fb0aa203d574d5671df1e99ee19a50
andrgje <aftgjesdal@gmail.com>
2022-05-04 10:30:51 +0200

    batch size

--- configs/textdet/dbnet/dbnet_r50dcnv2_fpnc_1200e_totaltext.py
+++ configs/textdet/dbnet/dbnet_r50dcnv2_fpnc_1200e_totaltext.py
@@ -14,8 +14,8 @@ test_pipeline_4068_1024 = {{_base_.test_pipeline_4068_1024}}
 
 load_from = 'data/backbone/dbnet_r50dcnv2_fpnc_sbn_2e_synthtext_20210325-aa96e477.pth'
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=10,
+    workers_per_gpu=8,
     val_dataloader=dict(samples_per_gpu=1),
     test_dataloader=dict(samples_per_gpu=1),
     train=dict(
