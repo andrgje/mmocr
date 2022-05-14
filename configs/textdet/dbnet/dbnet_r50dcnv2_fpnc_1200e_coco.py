639eb18b00dd628a35c06748d2b8c3ce5aadea14
andrgje <aftgjesdal@gmail.com>
2022-05-14 16:33:49 +0200

    fix save eval tag

--- configs/textdet/dbnet/dbnet_r50dcnv2_fpnc_1200e_coco.py
+++ configs/textdet/dbnet/dbnet_r50dcnv2_fpnc_1200e_coco.py
@@ -32,5 +32,5 @@ data = dict(
         datasets=test_list,
         pipeline=test_pipeline_4068_1024))
 
-evaluation = dict(interval=1, metric='hmean-iou', save_best='0_hmean-iou_hmean', rule='greater')
+evaluation = dict(interval=1, metric='hmean-iou', save_best='hmean-iou_hmean', rule='greater')
     
